20d5056e170f20a2071eedd823fa3aaaaa090ce5
Aldsouki <115744982+Aldsouki@users.noreply.github.com>
2024-09-08 16:46:58 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+import 'package:todo_app/views/home_page.dart';
+
+void main() {
+  runApp(ToDoApp());
+}
+
+class ToDoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
